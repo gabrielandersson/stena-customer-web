ac38285189111fd6ad252d97390eed0a77d55219
Gabrielandersson <andersson.b.gabriel@gmail.com>
2021-03-04 14:44:53 +0100

    Add i18next

---
+++ src/i18n.js
@@ -0,0 +1,18 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next'
+import HttpApi from "i18next-http-backend";
+
+
+i18n
+    .use(initReactI18next)
+    .use(HttpApi)
+    .use(LanguageDetector)
+
+    .init({
+
+    });
+
+
+export default i18n
+
